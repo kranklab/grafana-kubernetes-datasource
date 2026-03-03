9cb138fc9642fbe2bd86364fe30c4c6f55bff710
sdonnell <samdonnelly123@gmail.com>
2026-03-03 23:51:57 +0100

    Enhance CI workflow conditions and refine Kubernetes datasource tests

--- tests/configEditor.spec.ts
+++ tests/configEditor.spec.ts
@@ -12,7 +12,10 @@ test('"Save & test" should be successful when configuration is valid', async ({
   });
   const configPage = await createDataSourceConfigPage({ type: ds.type });
   await page.getByRole('textbox', { name: 'Kubernetes URL' }).fill(ds.jsonData.url ?? '');
+  await page.getByRole('radio', { name: 'Bearer token' }).click();
   await page.getByRole('textbox', { name: 'Bearer token' }).fill(ds.secureJsonData?.bearerToken ?? '');
+  await page.getByRole('textbox', { name: 'CA cert file' }).fill(ds.jsonData.caCert ?? '');
+  await configPage.mockHealthCheckResponse({ status: 'OK', message: 'Connected' });
   await expect(configPage.saveAndTest()).toBeOK();
 });
 
