cc97332fe76bfe1aec0c047689dad8223099f52e
sdonnell <samdonnelly123@gmail.com>
2026-03-03 18:39:23 +0100

    Enable client certificate support and enhance Kubernetes datasource tests

--- tests/queryEditor.spec.ts
+++ tests/queryEditor.spec.ts
@@ -8,6 +8,6 @@ test('should trigger new query when Resource field is changed', async ({
   await panelEditPage.datasource.set(ds.name);
   await panelEditPage.getQueryEditorRow('A').getByRole('textbox', { name: 'Action' }).fill('list');
   const queryReq = panelEditPage.waitForQueryDataRequest();
-  await panelEditPage.getQueryEditorRow('A').getByRole('textbox', { name: 'Resource' }).fill('pods');
+  await panelEditPage.getQueryEditorRow('A').getByRole('textbox', { name: 'Resource' }).fill('services');
   await expect(await queryReq).toBeTruthy();
 });
